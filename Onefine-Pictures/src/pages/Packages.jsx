6a1dd05f9cf253ff4fe3bd442b5b43f3f15da171
ClapClap_Tech <Kenjieemmanuellagaras@gmail.com>
2026-02-09 16:57:05 +0800

    Packages button added for separation of packages

--- Onefine-Pictures/src/pages/Packages.jsx
+++ Onefine-Pictures/src/pages/Packages.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import PackagesHeader from '../components/packages/PackagesHeader'
-import GraduationPackage from '../components/packages/GraduationPackage'
-import FamilyPackages from '../components/packages/FamilyPackages'
+import React from 'react';
+import PackagesHeader from '../components/packages/PackagesHeader';
+import GraduationPackage from '../components/packages/GraduationPackage';
+import FamilyPackages from '../components/packages/FamilyPackages';
+import Button from '../components/ui/Button'; // your Button component
 
 const Packages = () => {
   return (
     <div>
       <PackagesHeader />
-      
+
+      {/* Buttons above GraduationPackage */}
+      <div className="flex gap-4 mb-6 mt-6 justify-center">
+        <Button className="rounded-full bg-red-600 text-white group">
+          Graduation Packages
+        </Button>
+        <Button className="rounded-full bg-red-600 text-white group">
+          Group Packages
+        </Button>
+        <Button className="rounded-full bg-red-600 text-white group">
+          Family Packages
+        </Button>
+      </div>
+
       <GraduationPackage />
-
     </div>
-  )
-}
+  );
+};
 
-export default Packages
+export default Packages;
