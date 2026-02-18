4c7ae026184f6927464a2dac6a6d4c57e2f5a9d6
ClapClap_Tech <Kenjieemmanuellagaras@gmail.com>
2026-02-18 14:05:26 +0800

    hero section responsive

--- Onefine-Pictures/src/components/homeSections/HeroSection.jsx
+++ Onefine-Pictures/src/components/homeSections/HeroSection.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import Button from '../ui/Button'
-import Section from '../Section'
+import React from 'react';
+import Button from '../ui/Button';
+import Section from '../Section';
 
 const Hero = () => {
   return (
-    <Section fullScreen className="flex justify-center -mt-20">
+    <Section fullScreen className="relative flex justify-center -mt-20">
 
-      <div className="absolute inset-0 bg-cover bg-center"
-      style={{
-        backgroundImage: "url('/hero.png')",
-      }}
+      {/* Background Image */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/hero.png')" }}
       ></div>
 
-      <div className="relative z-10 text-center max-w-3xl">
-        <h1 className="text-5xl md:text-6xl font-extrabold leading-tight text-shadow-lg">
+      <div className="absolute inset-0 bg-black/5" />
+
+      {/* Content */}
+      <div className="relative z-10 text-center max-w-3xl px-4 sm:px-6">
+        <h1 className="text-5xl sm:text-6xl md:text-6xl leading-snug md:leading-tight text-shadow-2xstext-5xl font-extrabold text-shadow-lg">
           Capture Your <span className="text-red-500">Moments</span>
         </h1>
 
-        <p className="mt-6 text-lg md:text-xl text-gray-500 text-shadow-lg">
+        <p className="mt-4 sm:mt-6 text-lg sm:text-xl max-w-xl mx-auto leading-relaxed text-shadow-lg">
           Timeless photography for life’s most meaningful moments. Emotional & Story-Driven
         </p>
 
-        <div className="mt-8 flex justify-center gap-4">
-          <Button className="bg-red-600 px-6 py-3 text-lg text-white">
+        <div className="mt-6 sm:mt-8 flex flex-col sm:flex-row justify-center gap-4">
+          <Button className="bg-red-600 px-6 py-3 text-lg text-white w-full sm:w-auto">
             Book Now
           </Button>
-          <Button className="bg-white text-red-600 *:backdrop-blur px-6 py-3 text-lg ">
+          <Button className="bg-white text-red-600 px-6 py-3 text-lg w-full sm:w-auto">
             View Gallery
           </Button>
         </div>
       </div>
 
     </Section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
